67af16ebd213cdd6b0d5a436e09be0cf7bb59802
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-05-19 22:52:29 -0300

    Migrate from tkinter to PyQt5
    * Ease cross platform compatibity
    * Running app in macOS does not require python3 to be installed

--- src/files_creator.py
+++ src/files_creator.py
@@ -1,46 +1,49 @@
-import sys
 import os
 import uuid
-import threading
+from PyQt5.QtCore import QObject
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import pyqtSlot
 
-class filesCreator():
+
+class FilesCreator(QObject):
+    sig_step = pyqtSignal(str, int)
+    sig_done = pyqtSignal()
+    sig_abort = pyqtSignal(str)
+
     def __init__(self, path, number_files, size_files, size_unit):
+        super().__init__()
         self._path = path
-        self._number_files = number_files
-        self._size_files = size_files
+        self._number_files = int(number_files)
+        self._size_files = int(size_files)
         self._size_unit = size_unit
         self.created_files = 0
         self.errorFlag = 0
         self.error = ""
-        self._stop_event = 0
+        self.__abort = False
         self._running = False
 
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True
-        thread.start()
 
-    def is_running(self):
-        return self._running
-
-    def run(self):
+    @pyqtSlot()
+    def work(self):
         self._running = True
         prefix = 1024 ** self._size_unit
-        file_size = int(self._size_files) * prefix
+        file_size = self._size_files * prefix
         
-        while self.created_files < int(self._number_files):
-            if self._stop_event == 1:
+        while self.created_files < self._number_files:
+            if self.__abort == True:
                 self._running = False
                 return
-
             file_name = str(uuid.uuid4())
             try:
                 with open(self._path+"/"+file_name, 'wb') as fout:
                     fout.write(os.urandom(file_size))
             except IOError as e:
-                self.error = e
-                self.errorFlag = 1
-                sys.exit(1)
+                self.sig_abort.emit(str(e))
+                return
             self.created_files+=1
-    
-    def stop(self):
-        self._stop_event = 1
+            self.sig_step.emit(file_name, self.created_files)
+        self.sig_done.emit()
+
+
+    def abort(self):
+        self.__abort = True
