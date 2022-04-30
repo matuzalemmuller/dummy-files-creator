d36d0218f4cae4031578a30994aecc519ac59135
Matuzalem Muller <matuzalemmuller@gmail.com>
2022-04-30 14:24:51 -0700

    Add main function and change tab order for UI

--- src/ui.py
+++ src/ui.py
@@ -1,8 +1,9 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import QtGui
 from PyQt5 import QtCore
-import sys
 from files_creator import FilesCreator
+import sys
 
 
 class About(QtWidgets.QDialog):
@@ -17,15 +18,19 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
         uic.loadUi("../lib/qt/MainWindow.ui", self)  # Load the .ui file
-        self.setWindowIcon(QtGui.QIcon("../icon.png"))
         # Initialize UI elements
-        self.creating_files = False  # Files are not being created
+        self.__creating_files = False  # Files are not being created
+        self.__created_files = 0
+        self.__total_files = 0
         self.__set_validator()
         self.__set_connect()
         self.__switch_log_options()
         self.__switch_progress_bar()
         self.__toggle_create_stop()
         self.show()  # Show the GUI
+
+    def __about_action(self):
+        About()
 
     # Connects Qt elements to action methods
     def __set_connect(self):
@@ -40,8 +45,13 @@ class Ui(QtWidgets.QMainWindow):
         self.checkbox_savelog.stateChanged.connect(self.__switch_log_options)
         self.action_about.triggered.connect(self.__about_action)
 
-    def __about_action(self):
-        About()
+    # Set validator for number-exclusive fields
+    def __set_validator(self):
+        number_regex = QtCore.QRegExp("[0-9]+")
+        number_validator = QtGui.QRegExpValidator(number_regex)
+        self.text_n_files.setValidator(number_validator)
+        self.text_size_files.setValidator(number_validator)
+        self.text_chunk_size.setValidator(number_validator)
 
     # Enables create button when all fields have values
     def __toggle_create_stop(self):
@@ -55,13 +65,39 @@ class Ui(QtWidgets.QMainWindow):
         else:
             self.button_create_stop.setDisabled(True)
 
-    # Set validator for number-exclusive fields
-    def __set_validator(self):
-        number_regex = QtCore.QRegExp("[0-9]+")
-        number_validator = QtGui.QRegExpValidator(number_regex)
-        self.text_n_files.setValidator(number_validator)
-        self.text_size_files.setValidator(number_validator)
-        self.text_chunk_size.setValidator(number_validator)
+    # Open native folder dialog to select where test files will be created
+    def __browse_files(self):
+        dialog = QtWidgets.QFileDialog()
+        path = str(
+            QtWidgets.QFileDialog.getExistingDirectory(dialog, "Select Directory")
+        )
+        self.text_path.setText(path)
+
+    # Show/hide log folder text field
+    def __switch_log_options(self):
+        if self.checkbox_savelog.isChecked():
+            self.widget_log.show()
+        else:
+            self.widget_log.hide()
+
+    # Open native folder dialog to select where log file will be saved
+    def __browse_log(self):
+        dialog = QtWidgets.QFileDialog()
+        path = str(
+            QtWidgets.QFileDialog.getExistingDirectory(dialog, "Select Directory")
+        )
+        self.text_logfilepath.setText(path)
+
+    # Action to button Stop
+    def __cancel_creation(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure?")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        cancel_code = msg.exec_()
+        if cancel_code == QMessageBox.Yes:
+            self._change_layout("Stopped")
 
     # Action to button Close/Quit
     def __close_app(self):
@@ -77,52 +113,18 @@ class Ui(QtWidgets.QMainWindow):
         else:
             sys.exit(0)
 
-    # Action to button Stop
-    def _cancel_creation(self):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setWindowTitle("Confirmation")
-        msg.setText("Are you sure?")
-        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        cancel_code = msg.exec_()
-        if cancel_code == QMessageBox.Yes:
-            self._change_layout("Stopped")
-
-    # Open native folder dialog to select where test files will be created
-    def __browse_files(self):
-        dialog = QtWidgets.QFileDialog()
-        path = str(
-            QtWidgets.QFileDialog.getExistingDirectory(dialog, "Select Directory")
-        )
-        self.text_path.setText(path)
-
-    # Open native folder dialog to select where log file will be saved
-    def __browse_log(self):
-        dialog = QtWidgets.QFileDialog()
-        path = str(
-            QtWidgets.QFileDialog.getExistingDirectory(dialog, "Select Directory")
-        )
-        self.text_logfilepath.setText(path)
-
-    # Show/hide log folder text field
-    def __switch_log_options(self):
-        if self.checkbox_savelog.isChecked():
-            self.widget_log.show()
-        else:
-            self.widget_log.hide()
-
-    # Show/hide file creation progress bar
-    def __switch_progress_bar(self):
-        if self.creating_files:
-            self.widget_progress_bar.show()
-        else:
-            self.widget_progress_bar.hide()
-
     # Steps taken when clicking on Create/Stop
     def __create_stop_clicked(self):
         if self.button_create_stop.text() == "Create":
+            self.__total_files = int(self.text_n_files.text())
+            self.__created_files = 0
             self.__change_ui(state="disabled")
-            f = FilesCreator()
+            # for i in range(self.__total_files):
+            #     self.__created_files += 1
+            #     self.__update_progress_bar()
+            #     time.sleep(1)
+            # self.__change_ui(state="enabled")
+            # f = FilesCreator()
             # files_created = 0
             # while files_created < int(self.text_n_files.text()):
             #     f.create_files(path=self.text_path.text(),
@@ -133,7 +135,23 @@ class Ui(QtWidgets.QMainWindow):
             #     )
             #     files_created += 1
         else:
+            self.__total_files = 0
+            self.__created_files = 0
+            # self.__cancel_creation()
             self.__change_ui(state="enabled")
+
+    # Show/hide file creation progress bar
+    def __switch_progress_bar(self):
+        if self.__creating_files:
+            self.widget_progress_bar.show()
+        else:
+            self.widget_progress_bar.hide()
+
+    def __update_progress_bar(self):
+        progress_bar_text = str(self.__created_files) + "/" + str(self.__total_files)
+        progress_bar_percent = int(self.__created_files * 100 / self.__total_files)
+        self.label_progress.setText(progress_bar_text)
+        self.progress_bar.setValue(progress_bar_percent)
 
     # Enables/disables fields when app is starts running or turns idle
     def __change_ui(self, state: str):
@@ -155,6 +173,7 @@ class Ui(QtWidgets.QMainWindow):
             self.combo_chunk_unit.setDisabled(True)
             self.button_create_stop.setText("Stop")
             self.button_close_quit.setText("Quit")
+            self.widget_progress_bar.show()
         else:
             self.label_path.setDisabled(False)
             self.label_n_files.setDisabled(False)
@@ -173,9 +192,13 @@ class Ui(QtWidgets.QMainWindow):
             self.combo_chunk_unit.setDisabled(False)
             self.button_create_stop.setText("Create")
             self.button_close_quit.setText("Close")
+            self.widget_progress_bar.hide()
         return
 
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
 
-app = QtWidgets.QApplication(sys.argv)
-window = Ui()
-app.exec_()
+if __name__=="__main__":
+    main()
